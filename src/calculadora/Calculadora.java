2e4d19b83d75a285d9f3512e5c64f6600c07b3cb
Tikissmikiss <josehercepreciado@hotmail.com>
2022-05-25 20:05:19 +0200

    Primer commit

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,59 @@
+package calculadora;
+
+/**
+ * La aplicación consistirá en una calculadora sencilla capaz de realizar
+ * las siguientes operaciones:
+ * 
+ *   1. Suma y resta de números reales.
+ *   2. Multiplicación y división de números reales.
+ *   3. Raíz cuadrada de números reales. Deberéis identificar un método
+ *      de cálculo aproximado, al no poder utilizar funciones de la
+ *      librería matemática.
+ *   4. Función exponencial con base e. Deberéis identificar un método de
+ *      cálculo aproximado, al no poder utilizar funciones de la librería
+ *      matemática.
+ * 
+ * Para las operaciones 3 y 4 no podrás hacer uso de ninguna librería
+ * matemática y deberás utilizar operaciones básicas como la suma, resta,
+ * multiplicación y división, empleando para ello las funciones de la
+ * propia calculadora implementadas en los pasos anteriores. La precisión
+ * alcanzada en los cálculos debe ser superior a 1e-3.
+ */
+
+public class Calculadora {
+
+    public static double sumar(double a, double b) {
+        return 0;
+    }
+
+    
+    public static double restar(double a, double b) {
+        return 0;
+    }
+
+    
+    public static double multiplicar(double a, double b) {
+        return 0;
+    }
+
+    
+    public static double dividir(double a, double b) {
+        return 0;
+    }
+
+    
+    public static double potencia(double base, int exp) {
+        return 0;
+    }
+
+
+    public static double raiz(double x) {
+        return 0;
+    }
+
+    
+    public static double exponencial(int exp) {
+        return 0;
+    }
+    
+}
---
+++ test/calculadora/CalculadoraTest.java
@@ -0,0 +1,61 @@
+package calculadora;
+
+import org.junit.Test;
+
+/**
+ * La aplicación consistirá en una calculadora sencilla capaz de realizar
+ * las siguientes operaciones:
+ * 
+ *   1.	Suma y resta de números reales.
+ *   2.	Multiplicación y división de números reales.
+ *   3.	Raíz cuadrada de números reales. Deberéis identificar un método 
+ *      de cálculo aproximado, al no poder utilizar funciones de la 
+ *      librería matemática.
+ *   4.	Función exponencial con base e. Deberéis identificar un método de 
+ *      cálculo aproximado, al no poder utilizar funciones de la librería 
+ *      matemática.
+ */
+public class CalculadoraTest {
+    
+    @Test
+    public void testDividir() {
+
+    }
+
+    
+    @Test
+    public void testMultiplicar() {
+
+    }
+
+    
+    @Test
+    public void testPotencia() {
+
+    }
+
+    
+    @Test
+    public void testRaiz() {
+
+    }
+
+    
+    @Test
+    public void testRestar() {
+
+    }
+
+    
+    @Test
+    public void testSumar() {
+
+    }
+
+    
+    @Test
+    public void testExponencial() {
+
+    }
+
+}
